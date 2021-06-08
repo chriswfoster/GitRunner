648d1b72b7e22f6f4df9ba3b90148a2047540fbc
Chris <chriswfoster@gmail.com>
2021-06-08 11:16:01 -0500

    First

---
+++ index.js
@@ -0,0 +1,15 @@
+import * as _ from 'lodash';
+import {APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler} from 'aws-lambda';
+
+export const hander = async (event, ) => {
+    const max = 10;
+    const val = _.random(max);
+
+
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(`WATAP, random val : ${val}`)
+    }
+    return response;
+};
